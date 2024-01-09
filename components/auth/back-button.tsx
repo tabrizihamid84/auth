f9de6dd2ca8b46e15437bd5aa07356feab5f6c42
tabrizihamid84 <tabrizihamid6367@gmail.com>
2024-01-09 07:03:20 +0330

    first commit

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
+interface BackButtonProps {
+  href: string;
+  label: string;
+}
+
+const BackButton = ({ href, label }: BackButtonProps) => {
+  return (
+    <Button variant="link" className="w-full font-normal" size="sm" asChild>
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
+};
+
+export { BackButton };
