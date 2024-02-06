902dc2a5354779180c4fa36e81e416152fecfabd
tabrizihamid84 <tabrizihamid6367@gmail.com>
2024-02-06 23:58:31 +0330

    03:33

--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
+import { getUserById } from "./data/user";
 import { db } from "./lib/db";
 import authConfig from "./auth.config";
+import { UserRole } from "@prisma/client";
 
 export const {
   handlers: { GET, POST },
@@ -11,16 +13,38 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id);
+    //   console.log(existingUser);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
     async session({ token, session }) {
-      console.log({ sessionToken: token, session });
+      // console.log({ sessionToken: token, session });
 
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
 
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     },
   },
